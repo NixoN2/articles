8d22d34219ce246c6d514f9efb6f4599c8cb060f
NixoN2 <45205783+NixoN2@users.noreply.github.com>
2021-01-06 01:07:54 +0500

    Основы рендеринга в React

---
+++ Основые рендеринга в React/src/App.js
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default MyComponent;
---
+++ Основые рендеринга в React/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyComponent from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
